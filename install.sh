97e8c9a365a0981acb6ad5be382fb56424ae1fa5
whizzzkid <me@nishantarora.in>
2018-02-21 14:57:12 -0700

    Updating reqs

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ fi
 # Script
 msg "Installation Started"
 
-INSTALL_PATH=$HOME/.opencv
+INSTALL_PATH="/usr/local/opencv"
 msg "OpenCV will be installed in $INSTALL_PATH"
 
 DOWNLOAD_PATH=$1
 msg "OpenCV will be downloaded in $DOWNLOAD_PATH"
 
-CUDA_PATH="/usr/local/cuda"
+CUDA_PATH="/usr/local/cuda-9.1"
 
 msg "Updating system before installing new packages."
 sudo apt -y update
@@ -29,31 +29,91 @@ sudo apt -y autoremove
 sudo apt -y autoclean
 
 msg "Installing build tools."
-sudo apt install -y build-essential cmake git
+sudo apt install -y                  \
+  build-essential                    \
+  cmake                              \
+  git
 
 msg "Installing GUI components."
-sudo apt install -y qt5-default libvtk6-dev python-vtk6
+sudo apt install -y                  \
+  libvtk6-dev                        \
+  python-vtk6                        \
+  qt5-default
 
 msg "Installing media I/O componenets."
-sudo apt install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
+sudo apt install -y                  \
+  libgdal-dev                        \
+  libgphoto2-dev                     \
+  libjasper-dev                      \
+  libjpeg-dev                        \
+  libopenexr-dev                     \
+  libpng-dev                         \
+  libtiff5-dev                       \
+  libwebp-dev                        \
+  zlib1g-dev
 
 msg "Installing video I/O components."
-sudo apt install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev libxine2
+sudo apt install -y                  \
+  libavcodec-dev                     \
+  libavformat-dev                    \
+  libdc1394-22-dev                   \
+  libopencore-amrnb-dev              \
+  libopencore-amrwb-dev              \
+  libswscale-dev                     \
+  libtheora-dev                      \
+  libv4l-dev                         \
+  libvorbis-dev                      \
+  libx264-dev                        \
+  libxine2                           \
+  libxine2-dev                       \
+  libxvidcore-dev                    \
+  yasm
+
+msg "Installing Streaming Components."
+sudo apt install -y                  \
+  gstreamer1.0-doc                   \
+  gstreamer1.0-libav                 \
+  gstreamer1.0-plugins-bad           \
+  gstreamer1.0-plugins-base          \
+  gstreamer1.0-plugins-good          \
+  gstreamer1.0-plugins-ugly          \
+  gstreamer1.0-tools                 \
+  libgstreamer1.0-0                  \
+  libgstreamer1.0-dev                \
+  libgstreamer-plugins-bad1.0-dev    \
+  libgstreamer-plugins-base1.0-dev   \
+  libgstreamer-plugins-good1.0-dev
 
 msg "Installing Linear Algebra and Parallelism libs."
-sudo apt install -y libtbb-dev libeigen3-dev
+sudo apt install -y                  \
+  libeigen3-dev                      \
+  libtbb-dev
 
 msg "Installing LAPACKE libs."
-sudo apt install -y liblapacke-dev checkinstall
+sudo apt install -y                  \
+  checkinstall                       \
+  liblapacke-dev
 
 msg "Installing SFM components."
-sudo apt install -y libgflags-dev libgoogle-glog-dev libatlas-base-dev libsuitesparse-dev
+sudo apt install -y                  \
+  libatlas-base-dev                  \
+  libgflags-dev                      \
+  libgoogle-glog-dev                 \
+  libsuitesparse-dev
 
 msg "Installing Python."
-sudo apt install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
+sudo apt install -y                  \
+  python-dev                         \
+  python-numpy                       \
+  python-tk                          \
+  python3-dev                        \
+  python3-numpy                      \
+  python3-tk
 
 msg "Installing JDK."
-sudo apt install -y ant default-jdk
+sudo apt install -y                  \
+  ant                                \
+  default-jdk
 
 msg "Installing Docs."
 sudo apt install -y doxygen
@@ -113,7 +173,7 @@ cmake \
   -D BUILD_SHARED_LIBS=ON                                                      \
 ..
 make -j $(($(nproc)+1))
-make -j $(($(nproc)+1)) test
+#make -j $(($(nproc)+1)) test
 msg "Installing Ceres Solver."
 sudo make -j $(($(nproc)+1)) install
 cd $DOWNLOAD_PATH
@@ -127,6 +187,7 @@ msg "Configuring OpenCV Make"
 cmake \
       -D BUILD_EXAMPLES=ON                                                     \
       -D BUILD_OPENCV_JAVA=OFF                                                 \
+      -D BUILD_OPENCV_JS=ON                                                    \
       -D BUILD_OPENCV_NONFREE=ON                                               \
       -D BUILD_OPENCV_PYTHON=ON                                                \
       -D BUILD_EXAMPLES=ON                                                     \
@@ -134,10 +195,12 @@ cmake \
       -D CMAKE_INSTALL_PREFIX=$INSTALL_PATH                                    \
       -D CUDA_FAST_MATH=1                                                      \
       -D CUDA_TOOLKIT_ROOT_DIR=$CUDA_PATH                                      \
+      -D ENABLE_CCACHE=ON                                                      \
       -D ENABLE_FAST_MATH=1                                                    \
       -D ENABLE_PRECOMPILED_HEADERS=OFF                                        \
       -D INSTALL_C_EXAMPLES=ON                                                 \
       -D INSTALL_PYTHON_EXAMPLES=ON                                            \
+      -D INSTALL_TESTS=ON                                                      \
       -D OPENCV_EXTRA_MODULES_PATH=$DOWNLOAD_PATH/opencv_contrib/modules       \
       -D OPENCV_TEST_DATA_PATH=$DOWNLOAD_PATH/opencv_extra/testdata            \
       -D VTK_DIR=/usr/lib/cmake/vtk-6.2                                        \
@@ -146,6 +209,7 @@ cmake \
       -D WITH_CUDA=ON                                                          \
       -D WITH_FFMPEG=ON                                                        \
       -D WITH_GDAL=ON                                                          \
+      -D WITH_GSTREAMER=ON                                                     \
       -D WITH_LIBV4L=ON                                                        \
       -D WITH_NVCUVID=ON                                                       \
       -D WITH_OPENCL=ON                                                        \
